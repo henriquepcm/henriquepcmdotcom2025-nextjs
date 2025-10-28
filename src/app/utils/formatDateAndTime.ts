af39959bd4eaf76b2d59dcf83acadb9a2fe89761
Henrique Pochmann <henriquepcm@gmail.com>
2025-10-28 16:55:25 -0300

    feat: add friendly utc date and time

---
+++ src/app/utils/formatDateAndTime.ts
@@ -0,0 +1,15 @@
+export default function formatDateAndTime(moment: string): string {
+  const timeZone = "UTC";
+
+  return (
+    new Date(moment).toLocaleString("en-US", {
+      timeZone: timeZone,
+      year: "numeric",
+      month: "short",
+      day: "numeric",
+      hour: "numeric",
+      minute: "numeric",
+      hour12: true,
+    }) + ` ${timeZone}`
+  );
+}
